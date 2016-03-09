e410aa49d9d9566b8e30e9f3b6b5338c401ff787
Axel Roest <axel@phlux.us>
2016-03-09 11:37:59 +0100

    Initial Commit

---
+++ RandomTester/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RandomTester
+//
+//  Created by Axel Roest on 09/03/16.
+//  Copyright © 2016 The App Academy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RandomTester/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RandomTester
+//
+//  Created by Axel Roest on 09/03/16.
+//  Copyright © 2016 The App Academy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
